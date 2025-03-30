c574ff0192e8698309d561c856636835cc011f88
Abhinav Thakur <thakurabhinav16160@gmail.com>
2025-03-30 07:18:21 +0530

    add

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,12 +1,30 @@
-const asyncHandler = (fn) => (req, res, next) => {
-    try {
-        
-    } catch (error) {
-        res.status(err.code || 500).json({
-            success: false,
-            message: error.message,
-        });
-        
+const asyncHandler = (requestHandler)=>{
+    (req,res,next) => {
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err));
     }
 }
 export default asyncHandler
+
+
+
+
+
+
+
+
+
+
+
+
+// const asyncHandler = (fn) => async(req, res, next) => {
+//     try {
+//         await fn(req, res, next);
+        
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message,
+//         });
+        
+//     }
+// }
